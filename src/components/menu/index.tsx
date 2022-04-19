5d06ec048c7bbb3cf4c9917927dff64b14441bfe
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-04-19 12:07:18 +0300

    see routeroutlet töötab tavaliste a href linkidega

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -5,31 +5,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile, faHome, faTable } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
-
 export default class Menu extends React.Component<{}> {
   render() {
     return (
       <Router>
-      <menu className="menu-visible">
-        <img src={Logo} className={styles.logo} />
-        <ul>
-          <li>
-            <Link to="/">
-              Wiseproov <FontAwesomeIcon icon={faHome} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/table">
-              Tabel <FontAwesomeIcon icon={faTable} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/article">
-              Artikkel <FontAwesomeIcon icon={faFile} />
-            </Link>
-          </li>
-        </ul>
-      </menu>
+        <menu className="menu-visible">
+          <img src={Logo} className={styles.logo} />
+          <ul>
+            <li>
+              <a href="/">
+                Wiseproov <FontAwesomeIcon icon={faHome} />
+              </a>
+            </li>
+            <li>
+              <a href="/table">
+                Tabel <FontAwesomeIcon icon={faTable} />
+              </a>
+            </li>
+            <li>
+              <a href="/article">
+                Artikkel <FontAwesomeIcon icon={faFile} />
+              </a>
+            </li>
+          </ul>
+        </menu>
       </Router>
     );
   }
