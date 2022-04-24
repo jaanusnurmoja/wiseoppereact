5cec57c3f1abdc5c212c7cd59ba6c49ced154874
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-04-24 22:27:49 +0300

    10 rida tabelis ettenähtud andmetega

--- src/components/content/data.module.tsx
+++ src/components/content/data.module.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import styles from "./content.module.css";
 import { ResultProps } from "./resultProps";
 import parse from "html-react-parser";
-import DataList from "./helpers/dataList";
+import DataList from "./helpers/dataList.helper";
 const allDataUrl = "https://midaiganes.irw.ee/api/list";
 const works = new DataList([]);
 export default function DataModule() {
@@ -23,16 +23,22 @@ export default function DataModule() {
     <table>
       <thead>
         <tr>
-          <th id="title">Title</th>
-          <th id="author">Author</th>
+          <th id="firstname">Eesnimi</th>
+          <th id="surname">Perekonnanimi</th>
+          <th id="sex">Sugu</th>
+          <th id="birthdate">Sündinud</th>
+          <th id="phone">Telefon</th>
         </tr>
       </thead>
       <tbody>
         {result.map((value) => {
           return (
             <tr>
+              <td>{value.firstname}</td>
+              <td>{value.surname}</td>
+              <td>{value.sex === "m" ? "mees" : "naine"}</td>
               <td>{value.birthdate}</td>
-              <td>{value.author}</td>
+              <td>{value.phone}</td>
             </tr>
           );
         })}
